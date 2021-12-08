00f8a6ca6eeb6e9c9c2960384f6822a00daf52b2
xuanmie <xuanmie@tsign.cn>
2021-12-09 00:40:15 +0800

    fix

--- grab/process.go
+++ grab/process.go
@@ -36,9 +36,9 @@ func GetThreads(pid int32, threshold float64) []*SubThread {
 	// 获取进程的状态
 	go func() { getParentThreadState(pid, getParentThreadStateResult) }()
 	// 获取所有的子进程
-	thread := getSubThread(pid)
+	//thread := getSubThread(pid)
 	// 获取子进程详情
-	detailSubThread := getThreadDetail(thread)
+	detailSubThread := getThreadDetail2(int(pid), "vale")
 	if len(detailSubThread) <= 0 {
 		log.Println("子线程数量为0, 不需要这个工具了.")
 		return nil
@@ -69,7 +69,52 @@ func GetThreads(pid int32, threshold float64) []*SubThread {
 	return detailSubThread
 }
 
+/*
+29630 vale      20   0 6697940  91448  12452 S 13.3  0.6   1:52.74 Thread-1
+29632 vale      20   0 6697940  91448  12452 S 13.3  0.6   1:55.23 Thread-3
+29635 vale      20   0 6697940  91448  12452 S 13.3  0.6   1:55.16 Thread-6
+29629 vale      20   0 6697940  91448  12452 S  6.7  0.6   1:49.73 Thread-0
+29631 vale      20   0 6697940  91448  12452 R  6.7  0.6   1:53.50 Thread-2
+29633 vale      20   0 6697940  91448  12452 R  6.7  0.6   1:50.87 Thread-4
+29636 vale      20   0 6697940  91448  12452 S  6.7  0.6   1:49.94 Thread-7
+29637 vale      20   0 6697940  91448  12452 S  6.7  0.6   1:56.87 Thread-8
+29617 vale      20   0 6697940  91448  12452 S  0.0  0.6   0:00.00 java
+29618 vale      20   0 6697940  91448  12452 S  0.0  0.6   0:00.06 java
+*/
+func getThreadDetail2(goPid int, user string) []*SubThread {
+	shell := fmt.Sprintf("(top -n 1 -Hp %s | grep %s | head -10 | sed 's/\\x1b\\x28\\x42\\x1b\\[m//' | sed 's/\\x1b\\[1m//' | sed s/[[:space:]]/\\ /g)", goPid, user )
+	command := exec.Command("bash", "-c", shell)
+
+	// 可能没权限.
+	row, err := command.CombinedOutput()
+	if err != nil {
+		log.Println("top fail ", err)
+	}
+	subThreads := []*SubThread{}
+	lines := strings.Split(string(row), "\n")
+	for _, line := range lines {
+		arr := strings.Split(line, " ")
+		atoi, err := strconv.Atoi(arr[0])
+		if err != nil {
+			return nil
+		}
+		float, err := strconv.ParseFloat(arr[8], 32)
+		if err != nil {
+			return nil
+		}
+		sub := &SubThread{
+			pid: atoi,
+			pid16: fmt.Sprintf("%x", arr[0]),
+			CPUPercent: float,
+		}
+		subThreads = append(subThreads, sub)
+	}
+	return subThreads
+
+}
+
 func getThreadDetail(threads []int) []*SubThread {
+
 	subThreads := []*SubThread{}
 	stop := make(chan string)
 	wg := sync.WaitGroup{}
