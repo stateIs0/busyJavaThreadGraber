716166ab6c79aa23a680c2180aa992b37c7e0e67
xuanmie <xuanmie@tsign.cn>
2021-12-08 17:07:01 +0800

    fix cy

--- grab/process.go
+++ grab/process.go
@@ -5,19 +5,21 @@ import (
 	"github.com/shirou/gopsutil/process"
 	"log"
 	"strconv"
+	"time"
 )
 
 func GetThreads(pid int32, threshold float64) []string {
-	var rootProcess *process.Process
-	processes, _ := process.Processes()
-	for _, p := range processes {
-		if p.Pid == pid {
-			rootProcess = p
-			break
-		}
+
+	newProcess, err := process.NewProcess(pid)
+	if err != nil {
+		return nil
 	}
 
-	percent, err := rootProcess.CPUPercent()
+	percent, err := newProcess.Percent(3 * time.Second)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
 
 	log.Println("pid ", pid, " rootProcess CPUPercent = ", percent, ", threshold=", threshold)
 
@@ -33,7 +35,7 @@ func GetThreads(pid int32, threshold float64) []string {
 	threads := []string{}
 
 	if percent > threshold {
-		children, _ := rootProcess.Children()
+		children, _ := newProcess.Children()
 		fmt.Println("children:", children)
 		for _, p := range children {
 			cpuPercent, err := p.CPUPercent()
